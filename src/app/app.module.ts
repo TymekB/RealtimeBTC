cd05fafc701cc505fa83b523a000115db443b79c
Tymoteusz Baran <tb@Tymoteuszs-Mac-mini.local>
2020-02-02 23:26:31 +0100

    added BitcoinPriceComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BitcoinPriceComponent } from './bitcoin-price/bitcoin-price.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        BitcoinPriceComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/bitcoin-price/bitcoin-price.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+    selector: 'bitcoin-price',
+    templateUrl: './bitcoin-price.component.html',
+    styleUrls: ['./bitcoin-price.component.css']
+})
+export class BitcoinPriceComponent implements OnInit {
+
+    private price: number;
+
+    constructor(private http:HttpClient) { }
+
+    ngOnInit() {
+
+        this.http.get("https://api.coindesk.com/v1/bpi/currentprice.json").subscribe((res: any) => {
+            this.price = res.bpi.USD.rate_float;
+        });
+
+    }
+}
